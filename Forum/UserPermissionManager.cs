a7003688dab62860cf4446f3f49be922d0d81a7e
Michael Tran <mike_tran@live.com>
2013-12-08 12:36:44 -0500

    Added support for guest(not logged in) permissions

--- Forum/UserPermissionManager.cs
+++ Forum/UserPermissionManager.cs
@@ -20,34 +20,40 @@ namespace Forum
 
         public bool CanSeeForum(Database.Forum forum)
         {
-            if (user == null)
-                return true;
-
             return (GetEffectivePermissions(forum) & Permissions.ViewForum) != 0;
         }
 
         private Permissions GetEffectivePermissions(Database.Forum forum)
         {
-            var perForumGroupPermissions = user.UserGroup
-                .Join(db.PerForumGroupPermissions, ug => ug.GroupId, pfgp => pfgp.GroupId, (ug, pfgp) => pfgp)
+            IQueryable<Group> groupMembership;
+
+            if (user == null)
+            {
+                groupMembership = db.Group.Where(g => g.Name == "Guests");
+            }
+            else
+            {
+                var groupMembershipIds = user.UserGroup.Select(ug => ug.GroupId);
+                groupMembership = db.Group.Where(g => groupMembershipIds.Contains(g.Id));
+            }
+
+            var perForumGroupPermissions = groupMembership
+                .Join(db.PerForumGroupPermissions, g => g.Id, pfgp => pfgp.GroupId, (ug, pfgp) => pfgp)
                 .Where(pfgp => pfgp.ForumId == forum.Id);
 
             if (perForumGroupPermissions.Any())
             {
-                var combinedPermissions = perForumGroupPermissions
+                return perForumGroupPermissions
                     .Select(pfgp => (Database.Permissions)pfgp.Permissions)
+                    .ToList() // Aggregate in-memory, cannot do it on the IQueryable.
                     .Aggregate((state, cur) => state | cur);
-
-                return combinedPermissions;
             }
             else
             {
-                var combinedPermissions = user.UserGroup
-                    .Join(db.Group, ug => ug.GroupId, g => g.Id, (ug, g) => g)
+                return groupMembership
                     .Select(g => (Database.Permissions)g.Permissions)
+                    .ToList() // Aggregate in-memory, cannot do it on the IQueryable.
                     .Aggregate((state, cur) => state | cur);
-
-                return combinedPermissions;
             }
         }
     }
