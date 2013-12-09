d98ab897c035829728f9df279e573ac6aaebc515
Michael Tran <mike_tran@live.com>
2013-12-08 19:38:58 -0500

    Moved single category display in a partial view

---
+++ Forum/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forum.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public Database.Category Category { get; set; }
+        public UserPermissionManager PermissionManager { get; set; }
+    }
+}
