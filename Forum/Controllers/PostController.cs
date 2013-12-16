3be3e0d34bccf4970fc0c6fa01122253b5851b52
Michael Tran <mike_tran@live.com>
2013-12-15 20:54:59 -0500

    Added separation between a post and the first post of a thread

---
+++ Forum/Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forum.Controllers
+{
+    public class PostController : BaseController
+    {
+        //
+        // GET: /Post/Delete
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Post/Edit
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
