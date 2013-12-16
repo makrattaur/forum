b3f787604503dfe76a38c392b6bdf36146c0b414
Michael Tran <mike_tran@live.com>
2013-12-15 20:19:36 -0500

    Added ThreadViewModel

---
+++ Forum/ViewModels/ThreadViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forum.ViewModels
+{
+    public class ThreadViewModel
+    {
+        public Database.Thread Thread { get; set; }
+        public UserPermissionManager PermissionManager { get; set; }
+    }
+}
